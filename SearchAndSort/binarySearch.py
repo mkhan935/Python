a2b49e05740b39cbb44ab8b3d73270de5b83be04
Mohammed Khan <m.khan935@gmail.com>
2017-02-15 22:04:08 -0500

    binarySearch Iterative search reviewed

--- SearchAndSort/binarySearch.py
+++ SearchAndSort/binarySearch.py
@@ -12,11 +12,28 @@ def binarySearch(arr, left, right, x):
     else:
         return -1
 
+def IterativeBinarySearch(arr, left, right, x):
+    while left <= right:
+        middle = left + (right - left)//2
+
+        if arr[middle] == x:
+            return middle
+        elif arr[middle] < x:
+            left = middle + 1
+        else:
+            right = middle + 1
+    return -1
+
 testArr = [ 1, 2, 5, 15, 26 ]
 x=15
+y=2
 results = binarySearch(testArr,0,len(testArr)-1,x)
-
+Iresults = IterativeBinarySearch(testArr,0, len(testArr)-1,y)
 if results != -1:
     print ("Element is at index: %d " % results)
 else:
     print ("Element does not exist in array")
+if Iresults != -1:
+    print("Element is at index: %d " % Iresults)
+else:
+    print("Element does not exist in array")
