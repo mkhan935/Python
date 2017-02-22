c785c84a5e676968cf791eaf1e6cad999a101c2b
Mohammed Khan <m.khan935@gmail.com>
2017-02-21 22:29:14 -0500

    insertionSort reviewed

---
+++ SearchAndSort/insertionSort.py
@@ -0,0 +1,16 @@
+def insertionSort(arrToPassIn):
+    for i in range(1, len(arrToPassIn)):
+        key = arrToPassIn[i]
+
+        j=i-1
+        while j>=0 and key <arrToPassIn[j]:
+            arrToPassIn[j+1]=arrToPassIn[j]
+            j-=1
+        arrToPassIn[j+1]=key
+
+
+testArr = [12,11,13,5,6]
+insertionSort(testArr)
+print("sorted array: ")
+for item in range(len(testArr)):
+    print(testArr[item])
