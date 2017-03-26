ff64b60a6db3bd42a7a96d5ce58a137c477dbe2a
Mohammed Khan <m.khan935@gmail.com>
2017-03-26 16:26:08 -0400

    quicksort review

--- SearchAndSort/quick.py
+++ SearchAndSort/quick.py
@@ -5,7 +5,7 @@
 # array, and places all smaller (smaller than pivot)
 # to left of pivot and all greater elements to right
 # of pivot
-def partition(arr,low,high):
+def divideItUp(arr,low,high):
     i = ( low-1 )         # index of smaller element
     pivot = arr[high]     # pivot
 
@@ -22,28 +22,28 @@ def partition(arr,low,high):
     arr[i+1],arr[high] = arr[high],arr[i+1]
     return ( i+1 )
 
-# The main function that implements QuickSort
+
 # arr[] --> Array to be sorted,
 # low  --> Starting index,
 # high  --> Ending index
 
 # Function to do Quick sort
-def quickSort(arr,low,high):
+def quick(arr,low,high):
     if low < high:
 
-        # pi is partitioning index, arr[p] is now
+        # positioni is partitioning index, arr[p] is now
         # at right place
-        pi = partition(arr,low,high)
+        positioni = divideItUp(arr,low,high)
 
         # Separately sort elements before
         # partition and after partition
-        quickSort(arr, low, pi-1)
-        quickSort(arr, pi+1, high)
+        quick(arr, low, positioni-1)
+        quick(arr, positioni+1, high)
 
 # Driver code to test above
-arr = [10, 7, 8, 9, 1, 5]
-n = len(arr)
-quickSort(arr,0,n-1)
+Testarr = [123,32,43,1,3,56,4]
+n = len(Testarr)
+quick(Testarr,0,n-1)
 print ("Sorted array is:")
 for i in range(n):
-    print ("%d" %arr[i]),
+    print ("%d" %Testarr[i]),
